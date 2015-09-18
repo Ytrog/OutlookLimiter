c25c90c6a603d685d794ae0d434eae796932d073
Ytrog <hisbrucker@gmail.com>
2015-09-18 18:28:34 +0200

    init

---
+++ OutlookLimiter/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace OutlookLimiter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnOverride = new System.Windows.Forms.Button();
+            this.pbCountdown = new System.Windows.Forms.ProgressBar();
+            this.timerTicker = new System.Windows.Forms.Timer(this.components);
+            this.ttProgress = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(13, 13);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnOverride
+            // 
+            this.btnOverride.Location = new System.Drawing.Point(95, 13);
+            this.btnOverride.Name = "btnOverride";
+            this.btnOverride.Size = new System.Drawing.Size(75, 23);
+            this.btnOverride.TabIndex = 1;
+            this.btnOverride.Text = "Override";
+            this.btnOverride.UseVisualStyleBackColor = true;
+            // 
+            // pbCountdown
+            // 
+            this.pbCountdown.Location = new System.Drawing.Point(13, 43);
+            this.pbCountdown.Name = "pbCountdown";
+            this.pbCountdown.Size = new System.Drawing.Size(256, 23);
+            this.pbCountdown.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.pbCountdown.TabIndex = 2;
+            this.pbCountdown.MouseHover += new System.EventHandler(this.pbCountdown_MouseHover);
+            // 
+            // timerTicker
+            // 
+            this.timerTicker.Interval = 1000;
+            this.timerTicker.Tick += new System.EventHandler(this.timerTicker_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 90);
+            this.Controls.Add(this.pbCountdown);
+            this.Controls.Add(this.btnOverride);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Outlook limiter";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnOverride;
+        private System.Windows.Forms.ProgressBar pbCountdown;
+        private System.Windows.Forms.Timer timerTicker;
+        private System.Windows.Forms.ToolTip ttProgress;
+    }
+}
+
---
+++ OutlookLimiter/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OutlookLimiter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Process _process;
+        private const int _time = 1;
+        private bool _shownWarning = false;
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            _process = Process.Start("outlook.exe");
+            btnStart.Enabled = false;
+            StartTimer();
+        }
+
+        private void StartTimer()
+        {
+            
+            pbCountdown.Maximum = _time*60;
+            pbCountdown.Minimum = 0;
+            pbCountdown.Step = timerTicker.Interval/1000;
+            timerTicker.Start();
+
+        }
+
+        private void timerTicker_Tick(object sender, EventArgs e)
+        {
+
+            if (pbCountdown.Value < pbCountdown.Maximum)
+            {
+                pbCountdown.PerformStep();
+                
+                if (pbCountdown.Value >= (pbCountdown.Maximum - 60) && pbCountdown.Maximum > 60 && !_shownWarning)
+                {
+                    _shownWarning = true;
+                    MessageBox.Show("time almost up");
+                }
+            }
+            else
+            {
+                Stop();
+            }
+        }
+
+        private void pbCountdown_MouseHover(object sender, EventArgs e)
+        {
+            ttProgress.Show(String.Format("{0}/{1}", pbCountdown.Value, pbCountdown.Maximum), pbCountdown);
+        }        
+
+        private void Stop()
+        {
+            _process.CloseMainWindow();
+            btnStart.Enabled = true;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Stop();
+        }
+    }
+}
