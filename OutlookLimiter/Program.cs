80093b89eeb3409b210e956a31836f8b1a839630
Ytrog <hisbrucker@gmail.com>
2015-10-01 15:10:31 +0200

    single instance

--- OutlookLimiter/Program.cs
+++ OutlookLimiter/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,15 +9,29 @@ namespace OutlookLimiter
 {
     static class Program
     {
+
+        private static readonly Mutex _mutex = new Mutex(true, "{7A9F883F-BDF6-430E-9C97-69F0C50BE5AA}");
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+
+            if (!_mutex.WaitOne(TimeSpan.Zero, true)) return;
+
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+            finally
+            {
+                _mutex.ReleaseMutex();
+            }
         }
     }
 }
